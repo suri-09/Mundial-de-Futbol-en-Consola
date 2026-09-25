cc829ba7fd2fb899452cf8c5af2d7328ecdf8603
suri <surisaddai03@gmail.com>
2026-09-25 15:26:09 -0500

    Cambios en el codigo

--- Utilidades.java
+++ Utilidades.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 
 public class Utilidades {
 
-    // TODO: Terminar de agregar los paises.
-    // INFO: Posiblemente deberíamos cambiar el tamaño de la matriz.
-
     /**
      * Da el array con los colores de la bandera del pais dado.
      * @param pais Índice del país en el array.
      * @return Array bidimensional con los colores del país.
      */
+
     public static byte[][] banderas(int pais) {
         byte[][][] paises = {
                 // Colombia.
@@ -21,32 +19,32 @@ public class Utilidades {
                  { 2, 2, 2, 2, 2, 2, 2, 2, 2 },
                  { 1, 1, 1, 1, 1, 1, 1, 1, 1 }},
                 {{}}};
-
+                
         return paises[pais];
     }
 
     /**
-     * Diezma un array bidimensional según el factor dado, eliminando las filas y las columnas que están entre aquellas que sobreviven.
+     * Merma un array bidimensional según el factor dado, eliminando las filas y las columnas que están entre aquellas que sobreviven.
      * Si las columnas o las filas no son divisibles por el factor, se truncan las últimas.
      * @param array Array a diezmar.
      * @param factor Factor de diezmado.
      * @return Array diezmado.
      */
-    public static byte[][] decimate(byte[][] array, int factor) {
+    public static byte[][] mermar(byte[][] array, int factor) {
 
         // Da el tamaño del array tras ser diezmado.
         int filas = array.length / factor;
         int cols = array[0].length / factor;
 
-        byte[][] arrayDiezmado = new byte[filas][cols];
+        byte[][] arrayMermar = new byte[filas][cols];
 
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < cols; j++) {
-                arrayDiezmado[i][j] = array[i * factor][j * factor];
+                arrayMermar[i][j] = array[i * factor][j * factor];
             }
         }
 
-        return arrayDiezmado;
+        return arrayMermar;
     }
 
     /**
@@ -55,21 +53,21 @@ public class Utilidades {
      * @param factor Factor de interpolación.
      * @return Array interpolado.
      */
-    public static byte[][] interpolate(byte[][] array, int factor) {
+    public static byte[][] agregar(byte[][] array, int factor) {
 
         // Da el tamaño del array tras ser interpolado.
         int filas = array.length * factor;
         int cols = array[0].length * factor;
 
-        byte[][] arrayInterpolado = new byte[filas][cols];
+        byte[][] arrayAgregar = new byte[filas][cols];
 
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < cols; j++) {
-                arrayInterpolado[i][j] = array[i / factor][j / factor];
+                arrayAgregar[i][j] = array[i / factor][j / factor];
             }
         }
 
-        return arrayInterpolado;
+        return arrayAgregar;
     }
 
     public static class input {
